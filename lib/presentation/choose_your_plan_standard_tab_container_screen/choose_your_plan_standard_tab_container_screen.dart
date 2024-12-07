27c62235d2b7b6d1de4e6c2a8daf876237b0ad5c
Cesar Alvarez <159669801+Cesar-Alvarez13@users.noreply.github.com>
2024-12-07 03:13:50 -0600

    lISTO
    XD

--- lib/presentation/choose_your_plan_standard_tab_container_screen/choose_your_plan_standard_tab_container_screen.dart
+++ lib/presentation/choose_your_plan_standard_tab_container_screen/choose_your_plan_standard_tab_container_screen.dart
@@ -1,103 +1,229 @@
-import 'controller/choose_your_plan_standard_tab_container_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:diet_recipe_app/core/app_export.dart';
-import 'package:diet_recipe_app/presentation/choose_your_plan_customized_page/choose_your_plan_customized_page.dart';
-import 'package:diet_recipe_app/presentation/choose_your_plan_standard_one_page/choose_your_plan_standard_one_page.dart';
+import 'package:flutter/animation.dart';
 
-// ignore_for_file: must_be_immutable
-class ChooseYourPlanStandardTabContainerScreen
-    extends GetWidget<ChooseYourPlanStandardTabContainerController> {
-  const ChooseYourPlanStandardTabContainerScreen({Key? key})
-      : super(
-          key: key,
-        );
+class ChooseYourPlanStandardTabContainerScreen extends StatefulWidget {
+  const ChooseYourPlanStandardTabContainerScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChooseYourPlanStandardTabContainerScreenState createState() =>
+      _ChooseYourPlanStandardTabContainerScreenState();
+}
+
+class _ChooseYourPlanStandardTabContainerScreenState
+    extends State<ChooseYourPlanStandardTabContainerScreen> {
+  List<Food> foods = [
+    Food(
+      name: 'Frutas Frescas',
+      image: 'assets/images/frutas.jpg',
+      info: 'Las frutas frescas son ricas en vitaminas, fibra y antioxidantes, perfectas para una dieta saludable.',
+    ),
+    Food(
+      name: 'Yogur Natural',
+      image: 'assets/images/yogur.jpg',
+      info: 'El yogur natural es una fuente excelente de calcio y probióticos que ayudan a la digestión y salud ósea.',
+    ),
+    Food(
+      name: 'Verduras al Vapor',
+      image: 'assets/images/verduras_vapor.jpg',
+      info: 'Las verduras al vapor conservan más nutrientes y son bajas en calorías, ayudando a mantener un peso saludable.',
+    ),
+    Food(
+      name: 'Aguacate',
+      image: 'assets/images/aguacate.jpg',
+      info: 'El aguacate es una excelente fuente de grasas saludables, fibra y vitaminas que ayudan a la energía y el corazón.',
+    ),
+    Food(
+      name: 'Pechuga de Pollo',
+      image: 'assets/images/pechuga.jpg',
+      info: 'La pechuga de pollo es rica en proteínas y baja en grasa, ideal para el desarrollo muscular y la salud en general.',
+    ),
+    Food(
+      name: 'Pescado a la Parrilla',
+      image: 'assets/images/pescado.jpg',
+      info: 'El pescado es una fuente de ácidos grasos omega-3, importantes para la salud del cerebro y el sistema cardiovascular.',
+    ),
+  ];
+
+  List<bool> flipped = [];
+
+  @override
+  void initState() {
+    super.initState();
+    flipped = List<bool>.filled(foods.length, false);
+  }
 
   @override
   Widget build(BuildContext context) {
-    mediaQueryData = MediaQuery.of(context);
-
     return Scaffold(
-      backgroundColor: theme.colorScheme.onPrimaryContainer,
-      body: GetBuilder<ChooseYourPlanStandardTabContainerController>(
-        init: ChooseYourPlanStandardTabContainerController(),
-        builder: (controller) =>  Column(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'), // Imagen de fondo
+            fit: BoxFit.cover,  // Asegura que la imagen cubra toda la pantalla
+          ),
+        ),
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            // Título "Recomendaciones"
             Padding(
-              padding: getPadding(
-                left: 20,
-                bottom: 16,
-                top: 16,
-              ),
-              child: Text(
-                "msg_choose_your_plan".tr,
-                style: theme.textTheme.headlineMedium,
-              ),
-            ),
-
-            Stack(
-              fit: StackFit.passthrough,
-              alignment: Alignment.bottomCenter,
-              children: <Widget>[
-                Container(
-                  margin: getMargin(left: 20,right: 20,),
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: AppColor.grey200, width: getSize(2.5)),
+              padding: const EdgeInsets.all(16.0),
+              child: Card(
+                color: Colors.green,  // Fondo verde para el Card
+                elevation: 4.0,        // Sombra para darle un poco de profundidad
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0), // Bordes redondeados
+                ),
+                child: Container(
+                  alignment: Alignment.center,  // Centra el título
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),  // Añadir padding vertical para más espacio
+                  child: Text(
+                    'Recomendaciones', // Título
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,  // Texto en blanco
                     ),
                   ),
                 ),
-                TabBar(
-                    padding: getPadding(left: 20,right: 20),
-                    // isScrollable: true,
-                    // dividerColor: AppColor.primaryColor,
-                    indicatorColor: AppColor.primaryColor,
-                    labelStyle: TextStyle(
-                      color: AppColor.primaryColor,
-                      fontSize: getFontSize(16),
-                      fontWeight: FontWeight.w600,
-                    ),
-                    indicatorSize: TabBarIndicatorSize.tab,
-                    labelPadding: EdgeInsets.zero,
-                    indicatorPadding: EdgeInsets.zero,
-                    unselectedLabelColor: AppColor.grey400,
-                    unselectedLabelStyle: TextStyle(
-                        color: AppColor.grey400,
-                        fontWeight: FontWeight.w400,
-                        fontSize: getFontSize(16)
-                    ),
-
-                    controller: controller.tabviewController,
-                    // isScrollable: true,
-                    labelColor: theme.colorScheme.primary,
-
-                    tabs: [
-                      Tab(
-                        child:Text("Standard".tr,),
-                      ) ,
-                      Tab(child: Text("Customized".tr)),
-
-                    ]
-                ),
-              ],
+              ),
             ),
+
+            // Contenedor con GridView
             Expanded(
-              child: TabBarView(
-                controller: controller.tabviewController,
-                children: [
-                  ChooseYourPlanStandardOnePage(),
-                  ChooseYourPlanCustomizedPage(),
-                ],
+              child: Container(
+                color: Colors.white.withOpacity(0.7),  // Fondo blanco con opacidad para asegurar que las tarjetas sean legibles
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,  // Dos columnas
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                    childAspectRatio: 1,  // Hace que las tarjetas tengan proporciones cuadradas
+                  ),
+                  itemCount: foods.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        _showFoodInfoDialog(context, index); // Mostrar la ventana emergente
+                      },
+                      child: AnimatedSwitcher(
+                        duration: Duration(milliseconds: 500),
+                        child: Card(
+                          key: ValueKey('card_$index'),
+                          elevation: 8.0,
+                          color: Colors.green[300],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0), // Bordes redondeados
+                          ),
+                          child: Container(
+                            width: 150,  // Tamaño fijo en ambas direcciones para hacer el cuadrado
+                            height: 180, // Ajustamos la altura para hacer espacio para la imagen más grande
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16.0),
+                              color: Colors.white.withOpacity(0.1), // Fondo con opacidad
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                // Imagen más grande en la parte superior
+                                ClipRRect(
+                                  borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
+                                  child: Image.asset(
+                                    foods[index].image,
+                                    height: 100, // Imagen más grande
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      color: Colors.black.withOpacity(0.4), // Fondo opaco sin el efecto de cristal
+                                      borderRadius: BorderRadius.vertical(bottom: Radius.circular(16.0)),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        foods[index].name,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           ],
         ),
       ),
-
     );
   }
 
-  ///Handling route based on bottom click actions
+  // Función para mostrar la ventana emergente con la información
+  void _showFoodInfoDialog(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 500),
+            padding: EdgeInsets.all(16.0),
+            height: 300, // Ajusta la altura según el contenido
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  foods[index].name,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Image.asset(
+                  foods[index].image,
+                  width: 120,
+                  height: 120,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 16),
+                Text(
+                  foods[index].info,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black87,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
 
+class Food {
+  final String name;
+  final String image;
+  final String info;
+
+  Food({required this.name, required this.image, required this.info});
 }
--- lib/presentation/menu_page/menu_page.dart
+++ lib/presentation/menu_page/menu_page.dart
@@ -1,134 +1,226 @@
-import 'package:flutter/material.dart' hide MenuController;
-import 'package:diet_recipe_app/core/app_export.dart';
-import 'package:diet_recipe_app/presentation/menu_page/widgets/ourmenu_list.dart';
-import 'controller/menu_controller.dart';
-import 'models/menu_model.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
 
-// ignore_for_file: must_be_immutable
-class MenuPage extends StatelessWidget {
-  MenuPage({Key? key}) : super(key: key);
+class MenuPage extends StatefulWidget {
+  @override
+  _MenuPageState createState() => _MenuPageState();
+}
 
-  MenuPageController controller = Get.put(MenuPageController(MenuModel().obs));
+class _MenuPageState extends State<MenuPage> {
+  late List<IconData> _icons;
+  late List<bool> _cardFlipped;
+  late Timer _timer;
+  bool _gameStarted = false;
+  int _elapsedSeconds = 0;
+  int _matchesFound = 0;
+  List<int> _flippedIndexes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _initGame();
+  }
+
+  void _initGame() {
+    // Lista de íconos para el juego
+    _icons = [
+      Icons.favorite,
+      Icons.star,
+      Icons.cake,
+      Icons.ac_unit,
+      Icons.pets,
+      Icons.beach_access,
+    ];
+    _icons = [..._icons, ..._icons]; // Duplicar íconos para hacer pares
+    _icons.shuffle(); // Mezclar los íconos
+    _cardFlipped = List<bool>.filled(_icons.length, false);
+    _elapsedSeconds = 0;
+    _matchesFound = 0;
+    _flippedIndexes.clear();
+  }
+
+  void _startOrRestartGame() {
+    if (_gameStarted) {
+      // Reiniciar juego
+      _timer.cancel();
+      _initGame();
+      setState(() {
+        _gameStarted = false;
+      });
+    } else {
+      // Iniciar juego
+      setState(() {
+        _gameStarted = true;
+      });
+      _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+        setState(() {
+          _elapsedSeconds++;
+        });
+      });
+    }
+  }
+
+  void _flipCard(int index) {
+    if (_flippedIndexes.length >= 2 || _cardFlipped[index]) return;
+
+    setState(() {
+      _cardFlipped[index] = true;
+      _flippedIndexes.add(index);
+    });
+
+    if (_flippedIndexes.length == 2) {
+      Future.delayed(Duration(seconds: 1), () {
+        setState(() {
+          final firstIndex = _flippedIndexes[0];
+          final secondIndex = _flippedIndexes[1];
+          if (_icons[firstIndex] == _icons[secondIndex]) {
+            _matchesFound++;
+            if (_matchesFound == _icons.length ~/ 2) {
+              _timer.cancel();
+            }
+          } else {
+            _cardFlipped[firstIndex] = false;
+            _cardFlipped[secondIndex] = false;
+          }
+          _flippedIndexes.clear();
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    mediaQueryData = MediaQuery.of(context);
     return Scaffold(
-        backgroundColor: theme.colorScheme.onPrimaryContainer,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding:getPadding(top: 16,left: 20),
-                  child: Text('Our menu',style: TextStyle(
-                    color: AppColor.black,
-                    fontSize: getFontSize(28),
-                    fontWeight: FontWeight.w700,
-                  ),),
-                ),
-                Stack(
-                  fit: StackFit.passthrough,
-                  alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Container(
-                      margin: getMargin(left: 20,right: 20),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(color: AppColor.grey200, width: getSize(2.5)),
-                        ),
+      body: Stack(
+        children: [
+          // Fondo con la imagen
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/background.jpg',
+              fit: BoxFit.cover, // Ajusta la imagen para que cubra toda la pantalla
+            ),
+          ),
+          // Contenido del juego
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Card(
+                  color: Colors.green[800], // Fondo verde oscuro para la card
+                  margin: EdgeInsets.all(16.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Tiempo: $_elapsedSeconds segundos',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white, // Letra blanca
+                        shadows: [
+                          Shadow(
+                            blurRadius: 5.0,
+                            color: Colors.black,
+                            offset: Offset(2.0, 2.0),
+                          ),
+                        ], // Sombra para resaltar el texto
                       ),
                     ),
-                    TabBar(
-                        padding: getPadding(left: 20,right: 20),
-                        // isScrollable: true,
-                        // dividerColor: AppColor.primaryColor,
-                        indicatorColor: AppColor.primaryColor,
-                        labelStyle: TextStyle(
-                          color: AppColor.primaryColor,
-                          fontSize: getFontSize(16),
-                          fontWeight: FontWeight.w600,
-                        ),
-                        indicatorSize: TabBarIndicatorSize.tab,
-                        labelPadding: EdgeInsets.zero,
-                        indicatorPadding: EdgeInsets.zero,
-                        unselectedLabelColor: AppColor.grey400,
-                        unselectedLabelStyle: TextStyle(
-                            color: AppColor.grey400,
-                            fontWeight: FontWeight.w400,
-                            fontSize: getFontSize(16)
-                        ),
-
-                        controller: controller.tabviewController,
-                        // isScrollable: true,
-                        labelColor: theme.colorScheme.primary,
-
-                        tabs: [
-                          Tab(
-                            child:Text("lbl_main_course".tr,),
-
-                          ) ,
-                          Tab(child: Text("lbl_break_fast".tr)),
-                          Tab(child: Text("lbl_salad".tr)),
-                          Tab(child: Text("lbl_snack".tr))
-                        ]
-                    ),
-                  ],
+                  ),
                 ),
-                // Padding(
-                //   padding:getPadding(left: 20,top: 20),
-                //   child: Text("msg_60_lunch_dinner".tr, style: theme.textTheme.bodyLarge),
-                // ),
-                // Expanded(
-                // child: Padding(
-                //     padding: getPadding(top: 18),
-                //     child: ListView.separated(
-                //         physics: BouncingScrollPhysics(),
-                //         shrinkWrap: true,
-                //         separatorBuilder: (context, index) {
-                //           return SizedBox(height: getSize(16));
-                //         },
-                //         itemCount: AppListData.ourMenu.length,
-                //         itemBuilder: (context, index) {
-                //           OurMenuItemModel model = AppListData.ourMenu[index];
-                //              return OurMenuItemWidget(
-                //                model,
-                //                img: AppListData.ourMenu[index].img,
-                //                protein: AppListData.ourMenu[index].protein,
-                //                fat: AppListData.ourMenu[index].fat,
-                //                curb: AppListData.ourMenu[index].carb,
-                //                categories: AppListData.ourMenu[index].categories,
-                //                onTapOurmenu: () {
-                //                 onTapOurmenu();
-                //                 },
-                //
-                //             );
-                //           }
-                //         ),
-                //   ),
-                // ),
-                Expanded(
-                  child: TabBarView(
-
-                    controller: controller.tabviewController,
-                      children: [
-                    OurMenuList(),
-                    OurMenuList(),
-                    OurMenuList(),
-                    OurMenuList(),
-                  ]),
-                )
-
-          ]
+              ),
+              Expanded(
+                child: Center(
+                  child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    padding: EdgeInsets.symmetric(horizontal: 20), // Añadir un margen lateral
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,  // 3 columnas
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                      ),
+                      itemCount: _icons.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: _gameStarted ? () => _flipCard(index) : null,
+                          child: AnimatedSwitcher(
+                            duration: Duration(milliseconds: 500),
+                            transitionBuilder: (child, animation) {
+                              return RotationTransition(turns: animation, child: child);
+                            },
+                            child: _cardFlipped[index]
+                                ? Container(
+                                    key: ValueKey(true),
+                                    decoration: BoxDecoration(
+                                      color: Colors.green[800], // Carta verde oscuro cuando está volteada
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    child: Center(
+                                      child: Icon(
+                                        _icons[index],
+                                        size: 50, // Aumentar el tamaño del ícono
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  )
+                                : Container(
+                                    key: ValueKey(false),
+                                    decoration: BoxDecoration(
+                                      color: Colors.green[300], // Carta verde claro cuando está oculta
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        '${index + 1}',
+                                        style: TextStyle(color: Colors.white, fontSize: 24),
+                                      ),
+                                    ),
+                                  ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              if (!_gameStarted && _matchesFound == _icons.length ~/ 2)
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    '¡Juego terminado!\nTiempo final: $_elapsedSeconds segundos',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5, // 50% del ancho disponible
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green[800], // Verde oscuro
+                      foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                      padding: EdgeInsets.symmetric(vertical: 20), // Aumenta el tamaño vertical del botón
+                      textStyle: TextStyle(fontSize: 24), // Aumenta el tamaño del texto
+                    ),
+                    onPressed: _startOrRestartGame,
+                    child: Text(_gameStarted ? 'Reiniciar' : 'Iniciar'),
+                  ),
+                ),
+              ),
+            ],
           ),
-        )
+        ],
+      ),
     );
   }
 
-  onTapOurmenu() {
-    Get.toNamed(
-      AppRoutes.detailsScreen,
-    );
+  @override
+  void dispose() {
+    if (_gameStarted) {
+      _timer.cancel();
+    }
+    super.dispose();
   }
 }
