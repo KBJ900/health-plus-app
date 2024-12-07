2c01807f430e8587d9a01e68dd0bb0a17c3e9dd4
KBJ900 <kbjudemy@gmail.com>
2024-12-07 12:20:02 -0600

    Todo

--- lib/presentation/my_profile_screen/my_profile_screen.dart
+++ lib/presentation/my_profile_screen/my_profile_screen.dart
@@ -1,120 +1,340 @@
-// ignorar_por_archivo: uso_miembro_obsoleto
-
 import 'package:flutter/material.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart' as fs;
-import 'package:diet_recipe_app/core/app_export.dart';
-import 'package:diet_recipe_app/data/pref_data/pref_data.dart';
-import 'package:diet_recipe_app/presentation/my_profile_screen/widget/myprofile_ope.dart';
-import 'package:diet_recipe_app/widgets/app_bar_text.dart';
-import 'package:diet_recipe_app/widgets/custom_icon_button.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:uuid/uuid.dart';
 
-import '../../generated/assets.dart';
-import 'controller/my_profile_controller.dart';
+class MyProfileScreen extends StatefulWidget {
+  @override
+  _MyProfileScreenState createState() => _MyProfileScreenState();
+}
 
-// ignorar_por_archivo: debe_ser_inmutable
-class MyProfileScreen extends GetWidget<MyProfileController> {
-  MyProfileScreen({Key? key}) : super(key: key);
+class _MyProfileScreenState extends State<MyProfileScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nombreController = TextEditingController();
+  final TextEditingController _especialidadController = TextEditingController();
+  final TextEditingController _telefonoController = TextEditingController();
+  final TextEditingController _correoController = TextEditingController();
+  final TextEditingController _cedulaFileNameController = TextEditingController();
+  final TextEditingController _ineFileNameController = TextEditingController();
+  final TextEditingController _constanciaFileNameController = TextEditingController();
+  final TextEditingController _bancoFileNameController = TextEditingController();
+  final TextEditingController _domicilioFileNameController = TextEditingController();
 
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  File? _selectedCedula;
+  File? _selectedIne;
+  File? _selectedConstancia;
+  File? _selectedBanco;
+  File? _selectedDomicilio;
+
+  Future<void> _pickFile(String type) async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      setState(() {
+        switch (type) {
+          case 'cedula':
+            _selectedCedula = File(result.files.single.path!);
+            _cedulaFileNameController.text = result.files.single.name;
+            break;
+          case 'ine':
+            _selectedIne = File(result.files.single.path!);
+            _ineFileNameController.text = result.files.single.name;
+            break;
+          case 'constancia':
+            _selectedConstancia = File(result.files.single.path!);
+            _constanciaFileNameController.text = result.files.single.name;
+            break;
+          case 'banco':
+            _selectedBanco = File(result.files.single.path!);
+            _bancoFileNameController.text = result.files.single.name;
+            break;
+          case 'domicilio':
+            _selectedDomicilio = File(result.files.single.path!);
+            _domicilioFileNameController.text = result.files.single.name;
+            break;
+        }
+      });
+    }
+  }
+
+  Future<void> _submitProfile() async {
+    if (_formKey.currentState!.validate()) {
+      const url = 'https://health-back-lingering-wave-8458.fly.dev/api/users/doctors/doctorForm';
+      var request = http.MultipartRequest('POST', Uri.parse(url));
+
+      var uuid = Uuid();
+      String uid = uuid.v4().substring(0, 6);
+
+      request.fields['first_name'] = _nombreController.text;
+      request.fields['last_name'] = _especialidadController.text;
+      request.fields['specialty'] = _especialidadController.text;
+      request.fields['contact_number'] = _telefonoController.text;
+      request.fields['email'] = _correoController.text;
+      request.fields['uid'] = uid;
+
+      if (_selectedCedula != null) {
+        request.files.add(await http.MultipartFile.fromPath('urlCedula', _selectedCedula!.path));
+      }
+
+      if (_selectedIne != null) {
+        request.files.add(await http.MultipartFile.fromPath('urlIne', _selectedIne!.path));
+      }
+
+      if (_selectedConstancia != null) {
+        request.files.add(await http.MultipartFile.fromPath('urlConstancia', _selectedConstancia!.path));
+      }
+
+      if (_selectedBanco != null) {
+        request.files.add(await http.MultipartFile.fromPath('urlBanco', _selectedBanco!.path));
+      }
+
+      if (_selectedDomicilio != null) {
+        request.files.add(await http.MultipartFile.fromPath('urlDomicilio', _selectedDomicilio!.path));
+      }
+
+      // Log del payload que estás enviando
+      print('Fields: ${request.fields}');
+      if (_selectedCedula != null) print('Cedula path: ${_selectedCedula!.path}');
+      if (_selectedIne != null) print('INE path: ${_selectedIne!.path}');
+      if (_selectedConstancia != null) print('Constancia path: ${_selectedConstancia!.path}');
+      if (_selectedBanco != null) print('Banco path: ${_selectedBanco!.path}');
+      if (_selectedDomicilio != null) print('Domicilio path: ${_selectedDomicilio!.path}');
+
+      try {
+        var response = await request.send();
+        if (response.statusCode == 200) {
+          final responseBody = await response.stream.bytesToString();
+          print('Response: $responseBody');
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Perfil actualizado correctamente.')),
+          );
+          Navigator.pushNamed(context, '/home_screen_doctor');
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Error al actualizar el perfil.')),
+          );
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: $e')),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    mediaQueryData = MediaQuery.of(context);
-    return WillPopScope(
-      onWillPop: () {
-        return onTapBack();
-      },
-      child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          backgroundColor: theme.colorScheme.onPrimaryContainer,
-          body: SafeArea(
-            child: Form(
-                key: _formKey,
-                child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 11, 103, 116),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Regresar a la pantalla anterior
+          },
+        ),
+        title: const Text('Editar Perfil'),
+      ),
+      resizeToAvoidBottomInset: false, // No mover el contenido al abrir el teclado
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                // Subtítulo
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 11, 103, 116),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Editar Perfil',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Formulario
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      AppBarText(
-                        onTap: () => Get.back(),
-                        title: 'Mi perfil',
-                        rightIcon: CustomImageView(
-                          onTap: () {
-                            Get.toNamed(AppRoutes.editProfileScreen);
-                          },
-                          svgPath: Assets.imagesImgEdit,
-                          height: getSize(24),
+                      _buildTextFormField(_nombreController, 'Nombre Completo'),
+                      _buildTextFormField(_especialidadController, 'Especialidad'),
+                      _buildTextFormField(_telefonoController, 'Teléfono', keyboardType: TextInputType.phone),
+                      _buildTextFormField(_correoController, 'Correo Electrónico', keyboardType: TextInputType.emailAddress),
+                      SizedBox(height: 16),
+                      Center(
+                        child: Text(
+                          'Documentación',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
                         ),
                       ),
-                      Padding(
-                        padding: getPadding(top: 24, left: 20, right: 20,bottom: 32),
-                        child: Row(children: [
-                          Container(
-                              height: getSize(100),
-                              width: getSize(100),
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: fs.Svg(ImageConstant.imgAvtar1),
-                                      fit: BoxFit.cover)),
-                              child: CustomIconButton(
-                                  height: getSize(40),
-                                  width: getSize(40),
-                                  padding: getPadding(all: 8),
-                                  decoration: IconButtonStyleHelper.fillGrayTL20,
-                                  alignment: Alignment.bottomRight,
-                                  onTap: () {
-                                    onTapBtnClockone();
-                                  },
-                                  child: CustomImageView(
-                                      svgPath: ImageConstant.imgClockBlack900))),
-                          Padding(
-                              padding: getPadding(left: 15, top: 23, bottom: 23),
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Text("lbl_john_abram2".tr,
-                                        style: theme.textTheme.titleMedium),
-                                    Padding(
-                                        padding: getPadding(top: 11),
-                                        child: Text("msg_johnabram_gmail_com ".tr,
-                                            style: theme.textTheme.bodyLarge))
-                                  ]))
-                        ]),
+                      SizedBox(height: 16),
+                      GestureDetector(
+                        onTap: () => _pickFile('cedula'),
+                        child: AbsorbPointer(
+                          child: _buildFilePickerField(_cedulaFileNameController, 'Subir Cédula'),
+                        ),
                       ),
-
-                      MyProfileOpe(),
-                      MyProfileOpe(
-                        title: 'Apellido',
-                        personInfo: 'Abram xd',
+                      SizedBox(height: 8),
+                      GestureDetector(
+                        onTap: () => _pickFile('ine'),
+                        child: AbsorbPointer(
+                          child: _buildFilePickerField(_ineFileNameController, 'Subir INE'),
+                        ),
                       ),
-                      MyProfileOpe(
-                        personInfo: 'johnabram@gmail.com',
-                        title: 'Dirección de correo electrónico',
+                      SizedBox(height: 8),
+                      GestureDetector(
+                        onTap: () => _pickFile('constancia'),
+                        child: AbsorbPointer(
+                          child: _buildFilePickerField(_constanciaFileNameController, 'Subir Constancia'),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      GestureDetector(
+                        onTap: () => _pickFile('banco'),
+                        child: AbsorbPointer(
+                          child: _buildFilePickerField(_bancoFileNameController, 'Subir Banco'),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      GestureDetector(
+                        onTap: () => _pickFile('domicilio'),
+                        child: AbsorbPointer(
+                          child: _buildFilePickerField(_domicilioFileNameController, 'Subir Domicilio'),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: _submitProfile,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(255, 6, 122, 55),
+                            minimumSize: const Size(double.infinity, 60), // Altura fija de 60 px
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Text(
+                            'Guardar Cambios y Subir', // Texto del botón
+                            style: TextStyle(
+                              color: Colors.white, // Color del texto
+                              fontSize: 16, // Tamaño de la fuente
+                              fontWeight: FontWeight.bold, // Peso de la fuente
+                            ),
+                          ),
+                        ),
                       ),
                     ],
+                  ),
                 ),
+              ],
             ),
           ),
+        ),
       ),
     );
   }
 
-  onTapEditone() {
-    Get.toNamed(
-      AppRoutes.editProfileScreen,
+  Widget _buildTextFormField(TextEditingController controller, String label, {TextInputType keyboardType = TextInputType.text}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8, // 80% del ancho
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.7),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: TextFormField(
+          controller: controller,
+          keyboardType: keyboardType,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: TextStyle(color: Colors.grey),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none,
+            ),
+            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+          ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Este campo es obligatorio';
+            }
+            return null;
+          },
+        ),
+      ),
     );
   }
 
-  onTapBtnClockone() {
-    Get.toNamed(
-      AppRoutes.editProfileScreen,
-    );
-  }
-  onTapBack() {
-    PrefData.currentIndex = 4;
-    controller.update();
-    Get.toNamed(
-      AppRoutes.homeScreenContainerScreen,
+  Widget _buildFilePickerField(TextEditingController controller, String label) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8, // 80% del ancho
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.7),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: TextStyle(color: Colors.grey),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none,
+            ),
+            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            suffixIcon: Icon(Icons.attach_file),
+          ),
+          readOnly: true,
+        ),
+      ),
     );
   }
 }
