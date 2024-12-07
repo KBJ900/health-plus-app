9b260012fded0c04448e161a05c83d59a5fde601
KBJ900 <kbjudemy@gmail.com>
2024-12-07 14:02:48 -0600

    datalles

--- lib/presentation/running_plan_details_screen/running_plan_details_screen.dart
+++ lib/presentation/running_plan_details_screen/running_plan_details_screen.dart
@@ -1,11 +1,12 @@
 import 'package:diet_recipe_app/widgets/app_bar_text.dart';
-
 import 'controller/running_plan_details_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart' as fs;
 import 'package:diet_recipe_app/core/app_export.dart';
 import 'package:diet_recipe_app/widgets/custom_elevated_button.dart';
 import 'package:diet_recipe_app/widgets/custom_icon_button.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 // ignore_for_file: must_be_immutable
 class RunningPlanDetailsScreen extends GetWidget<RunningPlanDetailsController> {
@@ -20,14 +21,13 @@ class RunningPlanDetailsScreen extends GetWidget<RunningPlanDetailsController> {
 
     return Scaffold(
       backgroundColor: theme.colorScheme.onPrimaryContainer,
-
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             AppBarText(
-               onTap: () => Get.back(),
-              title: 'Make a grocery',
+              onTap: () => Get.back(),
+              title: 'Tu plan de alimentación',
             ),
             Container(
               margin: getMargin(top: 16),
@@ -82,8 +82,7 @@ class RunningPlanDetailsScreen extends GetWidget<RunningPlanDetailsController> {
                                   ),
                                   child: Text(
                                     "lbl_protein".tr,
-                                    style:
-                                        CustomTextStyles.bodyMediumBlack900,
+                                    style: CustomTextStyles.bodyMediumBlack900,
                                   ),
                                 ),
                                 Padding(
@@ -102,8 +101,7 @@ class RunningPlanDetailsScreen extends GetWidget<RunningPlanDetailsController> {
                                   ),
                                   child: Text(
                                     "lbl_carb".tr,
-                                    style:
-                                        CustomTextStyles.bodyMediumBlack900,
+                                    style: CustomTextStyles.bodyMediumBlack900,
                                   ),
                                 ),
                               ],
@@ -130,214 +128,84 @@ class RunningPlanDetailsScreen extends GetWidget<RunningPlanDetailsController> {
                 ),
               ),
             ),
-            Container(
-              margin: getMargin(
-                top: 24,
-                right: 20,left: 20
-              ),
-              padding: getPadding(
-                left: 16,
-                top: 18,
-                right: 16,
-                bottom: 18,
-              ),
-              decoration: AppDecoration.fillGray.copyWith(
-                borderRadius: BorderRadiusStyle.roundedBorder16,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "lbl_duration".tr,
-                        style: CustomTextStyles.bodyLargeOnError_1,
-                      ),
-                      Text(
-                        "lbl_1_month".tr,
-                        style: theme.textTheme.bodyLarge,
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: getPadding(
-                      top: 19,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "lbl_day_per_week".tr,
-                          style: CustomTextStyles.bodyLargeOnError_1,
-                        ),
-                        Text(
-                          "msg_5_days_sunday_off".tr,
-                          style: theme.textTheme.bodyLarge,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: getPadding(
-                      top: 19,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Padding(
-                          padding: getPadding(
-                            bottom: 1,
-                          ),
-                          child: Text(
-                            "lbl_start_date".tr,
-                            style: CustomTextStyles.bodyLargeOnError_1,
-                          ),
-                        ),
-                        Text(
-                          "lbl_sun_16_may".tr,
-                          style: theme.textTheme.bodyLarge,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: getPadding(
-                      top: 19,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Padding(
-                          padding: getPadding(
-                            bottom: 1,
-                          ),
-                          child: Text(
-                            "lbl_end_date".tr,
-                            style: CustomTextStyles.bodyLargeOnError_1,
-                          ),
-                        ),
-                        Text(
-                          "lbl_sun_16_may".tr,
-                          style: theme.textTheme.bodyLarge,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: getPadding(
-                      top: 19,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "lbl_plan_details".tr,
-                          style: CustomTextStyles.bodyLargeOnError_1,
-                        ),
-                        Text(
-                          "lbl_3_meals".tr,
-                          style: theme.textTheme.bodyLarge,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: getPadding(
-                      top: 14,
-                    ),
-                    child: Text(
-                      "msg_without_brakfast".tr,
-                      style: theme.textTheme.bodyLarge,
-                    ),
-                  ),
-                  Padding(
-                    padding: getPadding(
-                      top: 14,
-                    ),
-                    child: Text(
-                      "msg_2_salad_1_snack".tr,
-                      style: theme.textTheme.bodyLarge,
-                    ),
-                  ),
-                ],
+           Expanded(
+              child: FutureBuilder(
+                future: fetchMealPlans(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error al cargar los alimentos'));
+                  } else {
+                    final mealPlans = snapshot.data as List<dynamic>;
+                    return ListView.builder(
+                      padding: getPadding(top: 16),
+                      itemCount: mealPlans.length,
+                      itemBuilder: (context, index) {
+                        final mealPlan = mealPlans[index];
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: mealPlan['MealPlanFood'].map<Widget>((foodItem) {
+                            final food = foodItem['food'];
+                            return Padding(
+                              padding: getPadding(bottom: 16),
+                              child: Container(
+                                padding: getPadding(all: 16),
+                                decoration: AppDecoration.fillGray.copyWith(
+                                  borderRadius: BorderRadiusStyle.roundedBorder16,
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      food['name'],
+                                      style: theme.textTheme.titleMedium,
+                                    ),
+                                    Padding(
+                                      padding: getPadding(top: 8),
+                                      child: Text(
+                                        food['description'],
+                                        style: theme.textTheme.bodyLarge,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: getPadding(top: 8),
+                                      child: Text(
+                                        'Kcal: ${food['kcal']}, Fat: ${food['fat']}g, Protein: ${food['protein']}g, Carbs: ${food['carbs']}g',
+                                        style: theme.textTheme.bodyLarge,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        );
+                      },
+                    );
+                  }
+                },
               ),
             ),
-            Container(
-              width: double.infinity,
-              margin: getMargin(
-                top: 24,
-                left: 20,right: 20
-              ),
-              padding: getPadding(
-                all: 16,
-              ),
-              decoration: AppDecoration.fillGray.copyWith(
-                borderRadius: BorderRadiusStyle.roundedBorder16,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: getPadding(
-                      top: 4,
-                    ),
-                    child: Text(
-                      "lbl_payment_method".tr,
-                      style: theme.textTheme.titleMedium,
-                    ),
-                  ),
-                  Padding(
-                    padding: getPadding(
-                      top: 16,
-                    ),
-                    child: Row(
-                      children: [
-                        CustomIconButton(
-                          height: getSize(40),
-                          width: getSize(40),
-                          padding: getPadding(
-                            all: 8,
-                          ),
-                          decoration: IconButtonStyleHelper
-                              .fillOnPrimaryContainerTL16,
-                          child: CustomImageView(
-                            svgPath: ImageConstant.imgGooglepay11,
-                          ),
-                        ),
-                        Padding(
-                          padding: getPadding(
-                            left: 16,
-                            top: 11,
-                            bottom: 8,
-                          ),
-                          child: Text(
-                            "lbl_google_pay".tr,
-                            style: theme.textTheme.bodyLarge,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
         ),
       ),
-      bottomNavigationBar: CustomElevatedButton(
-        onTap: () => Get.back(),
-        text: "msg_download_invoice".tr,
-        margin: getMargin(
-          left: 20,
-          right: 20,
-          bottom: 40,
-        ),
-      ),
+
     );
+  }
+
+  Future<List<dynamic>> fetchMealPlans() async {
+    final url = Uri.parse('https://health-back-lingering-wave-8458.fly.dev/api/mealPlanRoutes/meal-plan/3');
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        return data['mealPlans'];
+      } else {
+        throw Exception('Error al cargar los planes de alimentación');
+      }
+    } catch (e) {
+      throw Exception('Error en la conexión');
+    }
   }
 }
